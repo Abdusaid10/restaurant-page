3d43b447feeca4b243891ffc5b95d7aa87faa36e
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-01-20 20:18:12 +0500

    added structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-
-console.log("hello")
+// Add all icons to the library so you can use it in your page
+library.add(fas, far, fab)
+import './style.css';
+// console.log("hello")
