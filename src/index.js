2815ec5447bb9b92bde3e333443288fc30775b7a
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2020-01-25 12:23:42 +0500

    added slider

--- src/index.js
+++ src/index.js
@@ -7,3 +7,30 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fas, far, fab)
 import './style.css';
 // console.log("hello")
+
+
+let slideIndex =0;
+
+const showSlides =()=>{
+  const slides = document.getElementsByClassName("slides");
+  const dots = document.getElementsByClassName("dots");
+
+  for(let i =0; i< slides.length; i ++){
+    slides[i].style.display = "none"
+  }
+  slideIndex++;
+
+  if(slideIndex > slides.length){
+    slideIndex=1;
+  }
+
+  for(let i=0; i<dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display="block";
+  dots[slideIndex - 1].className+=" active";
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
